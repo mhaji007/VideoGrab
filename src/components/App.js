f7adb4d9cab7d796b6ed333c31a95a98f9113007
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-02-29 14:36:36 -0500

    Add semantic ui grid. Fix warnings

--- src/components/App.js
+++ src/components/App.js
@@ -24,8 +24,8 @@ class App extends React.Component {
     };
 
     onVideoSelect = (video) => {
-        console.log('From the App!', video );
-        this.setState({selectedVideo:video});
+        console.log('From the App!', video);
+        this.setState({selectedVideo: video});
     };
 
     render() {
@@ -33,8 +33,15 @@ class App extends React.Component {
             <div className="ui container">
 
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <VideoDetail video = {this.state.selectedVideo}/>
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column"><VideoDetail video={this.state.selectedVideo}/></div>
+                        <div className="five wide column"><VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/></div>
+                    </div>
+
+                </div>
+
             </div>
         );
     }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,7 +5,7 @@ const VideoList = ({videos, onVideoSelect}) => {
 
     const renderendList = videos.map((video) => {
 
-        return <VideoItem onVideoSelect={onVideoSelect} video={video}/>;
+        return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video}/>;
     });
     return <div className="ui relaxed divided list">{renderendList}</div>;
 }
