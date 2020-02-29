6968b97b11e08e1f04230e8254eb3ad309366c54
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-02-29 00:53:41 -0500

    Add deeply nested callbacks to identify each video on click

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import VideoItem from './VideoItem'
 
-const VideoList =  ({videos}) => {
+const VideoList =  ({videos, onVideoSelect}) => {
     
    const renderendList = videos.map((video) => {
 
-        return <VideoItem video={video}/>;
+        return <VideoItem onVideoSelect = {onVideoSelect} video={video}/>;
     });
     return <div className="ui relaxed divided list">{renderendList}</div>;
 }
