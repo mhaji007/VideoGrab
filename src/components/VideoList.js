2ffd11c3f98bbb1f1fb8e7ba39f4745a4ea93036
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-02-28 19:48:16 -0500

    Render video thumbnails

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,7 +5,7 @@ const VideoList =  ({videos}) => {
     
    const renderendList = videos.map((video) => {
 
-        return <VideoItem/>;
+        return <VideoItem video={video}/>;
     });
     return <div>{renderendList}</div>;
 }
